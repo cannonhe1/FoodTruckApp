0c26874816710f839ba354c1fa5a2365b457a89b
cannonhe1 <cannonhe1@gmail.com>
2018-07-28 23:14:59 -0400

    project commit

---
+++ FoodTruckApp/FoodTruckApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FoodTruckApp.Models;
+
+namespace FoodTruckApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
